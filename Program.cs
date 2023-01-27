355577da33fb2ebd5e379c3c0356e0f4ce55f223
Puto-et-vivet <122829348+Puto-et-vivet@users.noreply.github.com>
2023-01-27 19:56:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Console.WriteLine("Введите целое трёхзначное число");
+// int x = Convert.ToInt32(Console.ReadLine());
+// int a = x/100;
+// int b = x/10;
+// int c = b-a*10;
+//     Console.WriteLine($" {c} ");
+
+
