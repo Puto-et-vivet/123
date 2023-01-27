4629b6f4d9142daf5405259a6639038f9ef94920
Puto-et-vivet <122829348+Puto-et-vivet@users.noreply.github.com>
2023-01-28 01:58:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,3 +6,30 @@
 //     Console.WriteLine($" {c} ");
 
 
+
+// Console.WriteLine("Введите целое число:"); 
+// int x = Convert.ToInt32(Console.ReadLine()); 
+// int z = 0;
+//     if (x>100 && x<999)
+//     {
+//         z=x%10;
+//     }
+//     else if (x<100)
+//     {
+//         Console.WriteLine("У введенного целого числа отсутствует разряд сотен");
+//     }
+//     else if (x>1000)
+//     {
+//        z=(x/100)%10; 
+//     }
+    
+// Console.WriteLine($"{z}");
+
+// Console.WriteLine("Введите любое целое число от 1 до 7");
+// int week = Convert.ToInt32(Console.ReadLine());
+// if( week == 6 || week == 7)Console.WriteLine($"{week} - выходной");
+// else if (week>7) Console.WriteLine("Некорректно введенно число");
+// else
+// {
+//     Console.WriteLine($"{week} - рабочий день");
+// }
